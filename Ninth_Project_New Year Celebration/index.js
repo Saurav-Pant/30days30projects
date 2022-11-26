394c243f0093158dd86f5971a863ed0e81119563
saurav <sauravpant01@gmail.com>
2022-11-26 13:34:55 +0530

    Ninth project css and html

---
+++ Ninth_Project_New Year Celebration/index.js
@@ -0,0 +1,9 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const mins = document.getElementById("mins");
+const secs = document.getElementById("secs");
+
+const date = new Date();
+const d = new Date("January 01, 2023 12:00:00");
+
+console.log(d.getFullYear);
