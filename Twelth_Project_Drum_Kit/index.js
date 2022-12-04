5efb52be5f96692185ede1313429e2e22d3d5e2c
saurav <sauravpant01@gmail.com>
2022-12-04 23:14:13 +0530

    Totally copying code but learned some cool stuff.

--- Twelth_Project_Drum_Kit/index.js
+++ Twelth_Project_Drum_Kit/index.js
@@ -1,7 +1,25 @@
-let all = document.querySelectorAll(".box");
-console.log(all);
+function playSound(e) {
+  // selects the audio element where data-key="e.keyCode"
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-for (let i = 0; i < all.length; i++) {
+  //if there is no audio element with the corresponding keyCode, then don't do anything
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
 
-    
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+  key.classList.add("playing");
 }
+function removeTransition(e) {
+  //transitionend is triggered on a variety of properties, (border, box shadow, etc)
+  // just select one - in this case 'transform'
+  if (e.propertyName != "transform") return;
+  e.target.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key2) =>
+  key2.addEventListener("transitionend", removeTransition)
+);
+window.addEventListener("keydown", playSound);
