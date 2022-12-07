81bc3e5b4dcf39ef4a906f6898cbdffad62a9fee
saurav <sauravpant01@gmail.com>
2022-12-07 22:46:27 +0530

    Quiz app Added.

--- Thirteenth_Project_Quiz App/index.js
+++ Thirteenth_Project_Quiz App/index.js
@@ -1,40 +1,82 @@
 const myQuestions = [
   {
     question: "Who invented JavaScript?",
-    answers: {
-      a: "Douglas Crockford",
-      b: "Sheryl Sandberg",
-      c: "Brendan Eich",
-    },
+
+    a: "Douglas Crockford",
+    b: "Sheryl Sandberg",
+    c: "Brendan Eich",
+
     correctAnswer: "c",
   },
   {
     question: "Which one of these is a JavaScript package manager?",
-    answers: {
-      a: "Node.js",
-      b: "TypeScript",
-      c: "npm",
-    },
+
+    a: "Node.js",
+    b: "TypeScript",
+    c: "npm",
+
     correctAnswer: "c",
   },
   {
     question: "Which tool can you use to ensure code quality?",
-    answers: {
-      a: "Angular",
-      b: "jQuery",
-      c: "RequireJS",
-      d: "ESLint",
-    },
+
+    a: "Angular",
+    b: "jQuery",
+    c: "RequireJS",
+    d: "ESLint",
+
     correctAnswer: "d",
   },
   {
     question: "What is the famous language according to data of 2022?",
-    answers: {
-      a: "javascript",
-      b: "Java",
-      c: "Sass",
-      d: "Python",
-    },
+
+    a: "javascript",
+    b: "Java",
+    c: "Sass",
+    d: "Python",
+
     correctAnswer: "a",
   },
 ];
+
+let currentQues = 0;
+loadQuiz();
+
+function loadQuiz() {
+  const ques = document.getElementById("ques");
+  const currentQuesData = myQuestions[currentQues];
+
+  ques.innerText = currentQuesData.question;
+  const a_text = document.getElementById("opt_1");
+  const b_text = document.getElementById("opt_2");
+  const c_text = document.getElementById("opt_3");
+
+  a_text.innerText = currentQuesData.a;
+  b_text.innerText = currentQuesData.b;
+  c_text.innerText = currentQuesData.c;
+}
+
+function getSelected() {
+  const ansEls = document.querySelectorAll(".ans");
+  let answer = undefined;
+  ansEls.forEach((ans) => {
+    if (ans.checked) {
+      answer = ans.id;
+    }
+  });
+  return answer;
+}
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  const answer = getSelected();
+  console.log(answer);
+  if (answer) {
+    currentQues++;
+    if (currentQues < myQuestions.length) {
+      loadQuiz();
+    } else {
+      alert("You Finished the quiz");
+    }
+  }
+});
